b595d81087e64a75d4677b72a29d21d3b556bf18
unknown <davejang98@naver.com>
2022-02-20 08:25:42 +0900

    caver-js

--- src/klaytn/caver.js
+++ src/klaytn/caver.js
@@ -1,9 +1,5 @@
 import Caver from 'caver-js'
 
-const BAOBAB_TESTNET_RPC_URL = 'https://api.baobab.klaytn.net:8651/'
-
-const rpcURL = BAOBAB_TESTNET_RPC_URL
-
-const caver = new Caver(rpcURL)
+const caver = new Caver(window.klaytn)
 
 export default caver
