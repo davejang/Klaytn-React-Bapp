5f3370ddfefb1e40268bc6c69c893e0b72fa39af
unknown <davejang98@naver.com>
2022-03-04 02:45:49 +0900

    header

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+    render() {
+        return (
+            <div>
+                <nav>
+                    <ul>
+                        <li><a href="">List1</a></li>
+                        <li><a href="">List2</a></li>
+                        <li><a href="">List3</a></li>
+                        <li><a href="">List4</a></li>
+                        <li><a href="">List5</a></li>
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
+}
+
+export default Nav;
--- src/klaytn/caver.js
+++ src/klaytn/caver.js
@@ -2,8 +2,4 @@ import Caver from 'caver-js'
 
 const caver = new Caver(window.klaytn)
 
-function walletConnect (){
-    return caver;
-}
-
 export default caver;
